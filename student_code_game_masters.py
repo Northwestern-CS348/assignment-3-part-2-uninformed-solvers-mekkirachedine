9b8b9408e6e8de09150126989158ab36c8e926b7
mekkirachedine <mekkirachedine1@gmail.com>
2019-02-22 21:58:39 -0600

    Assignment 3 done

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -33,8 +33,52 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### student code goes here
-        pass
+        t1 = []
+        t2 = []
+        t3 = []
+        p1 = self.kb.kb_ask(parse_input("fact: (on ?X peg1)"))
+        if p1:
+            for bindings in p1:
+                for binding in bindings.bindings:
+                    t1.append(binding.constant.element)
+        p2 = self.kb.kb_ask(parse_input("fact: (on ?X peg2)"))
+        if p2:
+            for bindings in p2:
+                for binding in bindings.bindings:
+                    t2.append(binding.constant.element)
+        p3 = self.kb.kb_ask(parse_input("fact: (on ?X peg3)"))
+        if p3:
+            for bindings in p3:
+                for binding in bindings.bindings:
+                    t3.append(binding.constant.element)
+        gameState = [t1, t2, t3]
+
+        index = 0
+        while index < 3:
+            L = len(gameState[index])
+            ind = 0
+            while ind < L:
+                if gameState[index][ind] == 'disk1':
+                    gameState[index][ind] = 1
+                elif gameState[index][ind] == 'disk2':
+                    gameState[index][ind] = 2
+                elif gameState[index][ind] == 'disk3':
+                    gameState[index][ind] = 3
+                elif gameState[index][ind] == 'disk4':
+                    gameState[index][ind] = 4
+                elif gameState[index][ind] == 'disk5':
+                    gameState[index][ind] = 5
+                ind = ind+1
+            index = index+1
+
+        gameState[0].sort()
+        gameState[1].sort()
+        gameState[2].sort()
+        tuple0 = tuple(gameState[0])
+        tuple1 = tuple(gameState[1])
+        tuple2 = tuple(gameState[2])
+        gameStateT = (tuple0, tuple1, tuple2)
+        return gameStateT
 
     def makeMove(self, movable_statement):
         """
@@ -52,7 +96,58 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
+        moved = str(movable_statement.terms[0])
+        askthing = "fact: (onDisk " + moved + " ?X)"
+        below = self.kb.kb_ask(parse_input(askthing))
+        t1 = []
+        if below:
+            for bindings in below:
+                for binding in bindings.bindings:
+                    t1.append(binding.constant.element)
+            oldState = ["onDisk", movable_statement.terms[0], t1[0]]
+            f2 = Fact(Statement(oldState))
+            self.kb.kb_retract(f2)
+            newState = ["top", t1[0], movable_statement.terms[1]]
+            f1 = Fact(Statement(newState))
+            self.kb.kb_assert(f1)
+        else:
+            newState = ["empty", movable_statement.terms[1]]
+            f1 = Fact(Statement(newState))
+            self.kb.kb_assert(f1)
+
+        oldState = ["on", movable_statement.terms[0], movable_statement.terms[1]]
+        f2 = Fact(Statement(oldState))
+        self.kb.kb_retract(f2)
+        oldState = ["top", movable_statement.terms[0], movable_statement.terms[1]]
+        f2 = Fact(Statement(oldState))
+        self.kb.kb_retract(f2)
+        oldState = ["empty", movable_statement.terms[2]]
+        f2 = Fact(Statement(oldState))
+        self.kb.kb_retract(f2)
+
+        newpeg = str(movable_statement.terms[2])
+        askthing = "fact: (top ?x " + newpeg + ")"
+        below = self.kb.kb_ask(parse_input(askthing))
+        t1 = []
+        if below:
+            for bindings in below:
+                for binding in bindings.bindings:
+                    t1.append(binding.constant.element)
+            oldState = ["top", t1[0], movable_statement.terms[2]]
+            f2 = Fact(Statement(oldState))
+            self.kb.kb_retract(f2)
+            newState = ["onDisk", movable_statement.terms[0], t1[0]]
+            f1 = Fact(Statement(newState))
+            self.kb.kb_assert(f1)
+
+        newState = ["on", movable_statement.terms[0], movable_statement.terms[2]]
+        f1 = Fact(Statement(newState))
+        self.kb.kb_assert(f1)
+        newState = ["top", movable_statement.terms[0], movable_statement.terms[2]]
+        f1 = Fact(Statement(newState))
+        self.kb.kb_assert(f1)
+
+        self.getGameState()
         pass
 
     def reverseMove(self, movable_statement):
@@ -99,8 +194,196 @@ class Puzzle8Game(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### Student code goes here
-        pass
+        rr1 = []
+        rr2 = []
+        rr3 = []
+        cc1 = []
+        cc2 = []
+        cc3 = []
+        r1 = self.kb.kb_ask(parse_input("fact: (Y ?w pos1)"))
+        if r1:
+            for bindings in r1:
+                for binding in bindings.bindings:
+                    rr1.append(binding.constant.element)
+        r2 = self.kb.kb_ask(parse_input("fact: (Y ?w pos2)"))
+        if r2:
+            for bindings in r2:
+                for binding in bindings.bindings:
+                    rr2.append(binding.constant.element)
+        r3 = self.kb.kb_ask(parse_input("fact: (Y ?w pos3)"))
+        if r3:
+            for bindings in r3:
+                for binding in bindings.bindings:
+                    rr3.append(binding.constant.element)
+        c1 = self.kb.kb_ask(parse_input("fact: (X ?w pos1)"))
+        if c1:
+            for bindings in c1:
+                for binding in bindings.bindings:
+                    cc1.append(binding.constant.element)
+        c2 = self.kb.kb_ask(parse_input("fact: (X ?w pos2)"))
+        if c2:
+            for bindings in c2:
+                for binding in bindings.bindings:
+                    cc2.append(binding.constant.element)
+        c3 = self.kb.kb_ask(parse_input("fact: (X ?w pos3)"))
+        if c3:
+            for bindings in c3:
+                for binding in bindings.bindings:
+                    cc3.append(binding.constant.element)
+
+        row1 = []
+        row2 = []
+        row3 = []
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr1[index] == cc1[ind]:
+                    row1.append(rr1[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr1[index] == cc2[ind]:
+                    row1.append(rr1[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr1[index] == cc3[ind]:
+                    row1.append(rr1[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr2[index] == cc1[ind]:
+                    row2.append(rr2[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr2[index] == cc2[ind]:
+                    row2.append(rr2[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr2[index] == cc3[ind]:
+                    row2.append(rr2[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr3[index] == cc1[ind]:
+                    row3.append(rr3[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr3[index] == cc2[ind]:
+                    row3.append(rr3[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+        stoploop = False
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if rr3[index] == cc3[ind]:
+                    row3.append(rr3[index])
+                    stoploop = True
+                    break
+                ind = ind + 1
+            if stoploop:
+                break
+            index = index + 1
+
+        gameState = [row1, row2, row3]
+        index = 0
+        while index < 3:
+            ind = 0
+            while ind < 3:
+                if gameState[index][ind] == 'tile1':
+                    gameState[index][ind] = 1
+                elif gameState[index][ind] == 'tile2':
+                    gameState[index][ind] = 2
+                elif gameState[index][ind] == 'tile3':
+                    gameState[index][ind] = 3
+                elif gameState[index][ind] == 'tile4':
+                    gameState[index][ind] = 4
+                elif gameState[index][ind] == 'tile5':
+                    gameState[index][ind] = 5
+                elif gameState[index][ind] == 'tile6':
+                    gameState[index][ind] = 6
+                elif gameState[index][ind] == 'tile7':
+                    gameState[index][ind] = 7
+                elif gameState[index][ind] == 'tile8':
+                    gameState[index][ind] = 8
+                elif gameState[index][ind] == 'empty':
+                    gameState[index][ind] = -1
+                ind = ind + 1
+            index = index + 1
+
+        tuple0 = tuple(gameState[0])
+        tuple1 = tuple(gameState[1])
+        tuple2 = tuple(gameState[2])
+        gameStateT = (tuple0, tuple1, tuple2)
+        return gameStateT
+
 
     def makeMove(self, movable_statement):
         """
@@ -118,8 +401,46 @@ class Puzzle8Game(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
+        emptyConstant = Constant('empty')
+        emptyTerm = Term(emptyConstant)
+
+        if movable_statement.terms[1].term.element != movable_statement.terms[3].term.element:
+            oldStateX = ["X", movable_statement.terms[0], movable_statement.terms[1]]
+            ffX = Fact(Statement(oldStateX))
+            self.kb.kb_retract(ffX)
+        if movable_statement.terms[2].term.element != movable_statement.terms[4].term.element:
+            oldStateY = ["Y", movable_statement.terms[0], movable_statement.terms[2]]
+            ffY = Fact(Statement(oldStateY))
+            self.kb.kb_retract(ffY)
+        if movable_statement.terms[1].term.element != movable_statement.terms[3].term.element:
+            oldStateX = ["X", emptyTerm, movable_statement.terms[3]]
+            ffX = Fact(Statement(oldStateX))
+            self.kb.kb_retract(ffX)
+        if movable_statement.terms[2].term.element != movable_statement.terms[4].term.element:
+            oldStateY = ["Y", emptyTerm, movable_statement.terms[4]]
+            ffY = Fact(Statement(oldStateY))
+            self.kb.kb_retract(ffY)
+
+        if movable_statement.terms[1].term.element != movable_statement.terms[3].term.element:
+            newStateX = ["X", movable_statement.terms[0], movable_statement.terms[3]]
+            fX = Fact(Statement(newStateX))
+            self.kb.kb_assert(fX)
+        if movable_statement.terms[2].term.element != movable_statement.terms[4].term.element:
+            newStateY = ["Y", movable_statement.terms[0], movable_statement.terms[4]]
+            fY = Fact(Statement(newStateY))
+            self.kb.kb_assert(fY)
+        if movable_statement.terms[1].term.element != movable_statement.terms[3].term.element:
+            newStateX = ["X", emptyTerm, movable_statement.terms[1]]
+            fX = Fact(Statement(newStateX))
+            self.kb.kb_assert(fX)
+        if movable_statement.terms[2].term.element != movable_statement.terms[4].term.element:
+            newStateY = ["Y", emptyTerm, movable_statement.terms[2]]
+            fY = Fact(Statement(newStateY))
+            self.kb.kb_assert(fY)
+
+        self.getGameState()
         pass
+
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -18,13 +18,39 @@ class SolverDFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+        if self.currentState.state == self.victoryCondition:
+            return True
+
+        self.visited[self.currentState] = True
+        if self.gm.getMovables():
+            i = 0
+            while i < len(self.gm.getMovables()):
+                movable = self.gm.getMovables()[i]
+                self.gm.makeMove(movable)
+                self.currentState.children.append(GameState(self.gm.getGameState(), self.currentState.depth+1, movable))
+                self.currentState.children[len(self.currentState.children)-1].parent = self.currentState
+                self.gm.reverseMove(movable)
+                i = i+1
+            for child in self.currentState.children:
+                if child not in self.visited.keys() or not self.visited[child]:
+                    self.gm.makeMove(child.requiredMovable)
+                    self.currentState = child
+                    return False
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+            return False
+        else:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+            return False
 
 
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+
+    q = []
+    tbc = 0
 
     def solveOneStep(self):
         """
@@ -39,5 +65,49 @@ class SolverBFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+
+        if self.currentState.state == self.victoryCondition:
+            self.q.clear()
+            self.tbc = 0
+            return True
+
+        if self.currentState.depth == 0:
+            self.q.append(self.currentState)
+        if self.gm.getMovables():
+            i = 0
+            while i < len(self.gm.getMovables()):
+                movable = self.gm.getMovables()[i]
+                self.gm.makeMove(movable)
+                newGameState = GameState(self.gm.getGameState(), self.currentState.depth + 1, movable)
+                isNewState = True
+                for a_state in self.q:
+                    if a_state == newGameState:
+                        isNewState = False
+                        break
+                if isNewState:
+                    self.q.append(GameState(self.gm.getGameState(), self.currentState.depth + 1, movable))
+                    self.q[len(self.q) - 1].parent = self.currentState
+                self.gm.reverseMove(movable)
+                i = i + 1
+        self.tbc = self.tbc + 1
+        while not self.currentState == self.q[0]:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+        if self.tbc < len(self.q):
+            pathM = []
+            pathS = []
+            endState = self.q[self.tbc]
+            pathS.append(endState)
+            pathM.append(endState.requiredMovable)
+            while not endState == self.q[0]:
+                endState = endState.parent
+                pathS.append(endState)
+                pathM.append(endState.requiredMovable)
+            pathS.reverse()
+            pathM.reverse()
+            i = 1
+            while not self.currentState == self.q[self.tbc]:
+                self.gm.makeMove(pathM[i])
+                self.currentState = pathS[i]
+                i = i+1
+            return False
